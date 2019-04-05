3c9b889f5b2346b54dfcfbb20eb0a7c0874d6ce2
Anne Wilson <annewilson2591@gmail.com>
2019-04-04 20:12:31 -0600

    init anne branch

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,41 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS align_db;
+CREATE DATABASE align_db
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+
+CREATE TABLE `calendars`
+(
+  `id` int PRIMARY KEY,
+  `owner` varchar(255),
+  `created_at` varchar(255),
+  `timeperiod` varchar(255),
+  `startdate` varchar(255),
+  `status` boolean
+);
+
+CREATE TABLE `users`
+(
+  `id` int PRIMARY KEY,
+  `full_name` varchar(255),
+  `created_at` varchar(255),
+  `status` boolean
+);
+
+CREATE TABLE `schedule`
+(
+  `id` int PRIMARY KEY,
+  `cal_id` int,
+  `use_id` int,
+  `monday` int,
+  `tuesday` int,
+  `wednesday` int,
+  `thursday` int,
+  `friday` int,
+  `saturdaty` int,
+  `sunday` int
+);
+
+ALTER TABLE `calendars` ADD FOREIGN KEY (`owner`) REFERENCES `users` (`id`);
+
+ALTER TABLE `schedule` ADD FOREIGN KEY (`cal_id`) REFERENCES `calendars` (`id`);
+
+ALTER TABLE `schedule` ADD FOREIGN KEY (`use_id`) REFERENCES `users` (`id`);
