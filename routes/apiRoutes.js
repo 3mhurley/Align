8df5a0f0a54f233a5d8af49f7df99c069a31e2a8
hyper linked <45010713+kyledkearney@users.noreply.github.com>
2019-04-04 21:07:58 -0600

    basic api routes - needs work on calendar and user input

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,44 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
+  // Get all times
+  app.get("/api/all-times", function(req, res) {
+    db.Times.findAll({}).then(function(dbTimes) {
+      res.json(dbTimes);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
+  // Create a new time
+  app.post("/api/newTime", function(req, res) {
+    db.Times.create(req.body).then(function(dbNewTime) {
+      res.json(dbNewTime);
     });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
+  // create a new user
+  app.post("/api/examples/:user", function(req, res) {
+    db.Calendar.create(req.pram.user).then(function(dbNewUser) {
+      res.json(dbNewUser);
     });
   });
-};
+
+  // create a new calendar
+  app.post("/api/calendar/:id", function(req, res) {
+    db.Calendar.create(req.param.id).then(function(calendardb) {
+      res.json(calendardb);
+    });
+  });
+  // get user information
+  app.get("/api/users", function(req, res) {
+    db.Calendar.findAll(users).then(function(dbGetUsers) {
+      res.json(dbGetUsers);
+    });
+  });
+
+    // Get Calendar
+    app.get("/api/calendar/:id", function(req, res) {
+      db.Calendar.findAll(calendars, {where:{calendarID: req.param.id}}).then(function(dbGetCalendar) {
+        res.json(dbGetCalendar);
+      });
+    });
+
