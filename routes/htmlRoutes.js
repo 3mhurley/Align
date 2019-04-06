4bd7e47e8aef3508e536a7b113bee2ad21960198
hyper linked <45010713+kyledkearney@users.noreply.github.com>
2019-04-06 10:06:47 -0600

    travis is a fucking dick

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -14,7 +14,7 @@ module.exports = function(app) {
   // Load example page and pass in an example by id
   app.get("/example/:id", function(req, res) {
     db.Example.findOne({
-      where:{
+      where: {
         id: req.params.id
       }
     }).then(function(dbExample) {
