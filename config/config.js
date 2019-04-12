edd0c8e29f64acf2c32b80a3afb6a88f971dbdf5
Jordan Choquette <j.w.choquette@gmail.com>
2019-04-11 21:03:30 -0600

    routing to calendar page done, modals are now working

--- config/config.js
+++ config/config.js
@@ -1,18 +1,17 @@
-require('dotenv').config();
-
+require("dotenv").config();
 
 module.exports = {
   development: {
     database: "align_db",
     username: "root",
-    password: process.env.PASSWORD,
+    password: "Bubba&Poppy1",
     host: "localhost",
     dialect: "mysql"
   },
   test: {
     database: "align_db",
     username: "root",
-    password: process.env.PASSWORD,
+    password: "Bubba&Poppy1",
     host: "localhost",
     dialect: "mysql",
     logging: false
@@ -20,7 +19,5 @@ module.exports = {
   production: {
     "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
-  },
+  }
 };
-
- 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,13 +4,12 @@ var path = require("path");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 
-  // load calendar page 
-  app.get("/calendar/:id", function(req, res) {
+  // load calendar page
+  app.get("/calendar", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/calendar.html"));
-
   });
 
   // Render 404 page for any unmatched routes
