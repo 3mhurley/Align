c265bfd15cb92ba2dfcad13027baa7df0a076976
Jordan Choquette <j.w.choquette@gmail.com>
2019-04-13 08:41:07 -0600

    datepicker modal now working

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,18 +1,18 @@
 // Homepage
 $(document).ready(function() {
   $(".modal").modal();
-  $("#selectDate").pickadate({
+  $("#selectDate").datepicker({
     container: ".dropPick",
     format: "mm/dd/yyyy",
     autoClose: "true"
   });
 
-  $(".pushpin-nav").each(function() {
-    var $this = $(this);
-    var $target = $("#" + $(this).attr("data-target"));
-    $this.pushpin({
-      top: $target.offset().top,
-      bottom: $target.offset().top + $target.outerHeight() - $this.height()
-    });
-  });
+  // $(".pushpin-nav").each(function() {
+  //   var $this = $(this);
+  //   var $target = $("#" + $(this).attr("data-target"));
+  //   $this.pushpin({
+  //     top: $target.offset().top,
+  //     bottom: $target.offset().top + $target.outerHeight() - $this.height()
+  //   });
+  // });
 });
